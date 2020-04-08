13fc81ddd4dd81a06837782f20c704d66e39e17a
amazingshellyyy <amazingshellyyy@gmail.com>
2020-04-08 10:22:17 -0700

    add unsplash background image

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-   <div>this is app</div>
-  );
+class App extends React.Component {
+    state = {
+        wallPaperLink: null
+    }
+
+    componentDidMount(){
+        axios.get('https://api.unsplash.com/photos/random/?client_id=N6gSTT5QTo0hwGeeZfW1HH59hwp5pBqRx2pH7EXsrLE&&collections=827743')
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    wallPaperLink: res.data.urls.full
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    render(){
+        return (
+            <div className="container" style={{backgroundImage:`url(${this.state.wallPaperLink})`}}>
+                <div className="row">
+                    
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
